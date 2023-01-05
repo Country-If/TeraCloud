5000d2aa1784c896b46bd3a75213f4047ab36fbd
Maylon <2450854008@qq.com>
2023-01-05 11:54:36 +0800

    create new file that start the main procedure

---
+++ main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+__author__ = "Maylon"
+
+from PyQt5.QtWidgets import QApplication
+import sys
+
+from main_login import Main_login_ui
+
+
+class Main:
+    def __init__(self):
+        self.__setup_ui()
+
+    def __setup_ui(self):
+        self.login_ui = Main_login_ui()
+        self.login_ui.ui.Login_btn.clicked.connect(self.login)
+
+    def login(self):
+        if self.login_ui.login_status:
+            self.login_ui.ui.close()
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = Main()
+    window.login_ui.ui.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
