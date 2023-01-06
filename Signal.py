ab274a290b845c699122a29693cbfa5bb8c3b917
Maylon <2450854008@qq.com>
2023-01-06 11:38:26 +0800

    upload Signal Class

---
+++ Signal.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+__author__ = "Maylon"
+
+from PyQt5.QtCore import pyqtSignal, QObject
+
+
+class MySignals(QObject):
+    """自定义信号类"""
+    inform_signal = pyqtSignal()
+    login_success_signal = pyqtSignal()
+    login_fail_signal = pyqtSignal()
