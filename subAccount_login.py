dad436dfac20c7af4056af90c09038f7a99bafc7
Maylon <2450854008@qq.com>
2023-01-09 16:05:45 +0800

    add subAccount UI

---
+++ subAccount_login.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+__author__ = "Maylon"
+
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+
+
+class SubAccount_login_ui:
+    """子账号登录"""
+
+    def __init__(self):
+        """
+        实例化对象
+        """
+        # 动态加载界面
+        self.ui = uic.loadUi("UI/subAccount_login.ui")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = SubAccount_login_ui()
+    window.ui.show()
+    app.exec_()
